c62d313a0530bef79163e348095af61ea5c43ffc
manogupt <manojgupta123929@gmail.com>
2022-06-19 18:48:11 +0530

    Added uc4

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -4,27 +4,27 @@ import java.util.Random;
 
 public class EmployeeWages 
 {
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-		int attendance = random.nextInt(3); // 0 or 1
+		int attendance = random.nextInt(3); // generate random number 0,1,2
 
 		int wagePerHour = 20;
 		int fullDayHour = 8;
 		int partTimeHours = 4;
 		int wagePerDay = 0;
 
-		if (attendance == 0)
+		switch (attendance) {
+		case 0:
 			System.out.println("Employee Absent ..");
-
-		else if (attendance == 1)
-
+			break;
+		case 1:
 			wagePerDay = partTimeHours * wagePerHour;
-
-		else
+			break;
+		default:
 			wagePerDay = fullDayHour * wagePerHour;
+		}
 
 		System.out.println("Employee wage per Day: " + wagePerDay);
 	}
