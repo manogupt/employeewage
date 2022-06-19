29beddfabfb212a83988ca3b8b0b7f7a7045477f
manogupt <manojgupta123929@gmail.com>
2022-06-19 18:52:47 +0530

    Added : uc5

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -8,24 +8,35 @@ public class EmployeeWages
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 		Random random = new Random();
-		int attendance = random.nextInt(3); // generate random number 0,1,2
 
 		int wagePerHour = 20;
 		int fullDayHour = 8;
 		int partTimeHours = 4;
 		int wagePerDay = 0;
+		int monthlySalary = 0;
 
-		switch (attendance) {
-		case 0:
-			System.out.println("Employee Absent ..");
-			break;
-		case 1:
-			wagePerDay = partTimeHours * wagePerHour;
-			break;
-		default:
-			wagePerDay = fullDayHour * wagePerHour;
+		for (int day = 1; day <= 20; day++) 
+		{
+			int attendance = random.nextInt(3); // generate random number 0,1,2
+			System.out.println("randomNumber" + attendance);
+			wagePerDay = 0;
+			
+			switch (attendance) {
+			case 0:
+				System.out.println("Employee Absent ..");
+				break;
+			case 1:
+				System.out.println("part time present");
+				wagePerDay = partTimeHours * wagePerHour;
+				break;
+			default:
+				System.out.println("Full day present");
+				wagePerDay = fullDayHour * wagePerHour;
+				break;
+			}
+			monthlySalary = monthlySalary + wagePerDay;
+			System.out.println(day + "  : " + monthlySalary);
 		}
 
-		System.out.println("Employee wage per Day: " + wagePerDay);
 	}
 }
